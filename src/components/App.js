dade92aba0406472e13631682f5890c63510e3dc
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-07-10 21:27:57 -0400

    Fetch TV and movie data

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,39 @@
 import "../css/App.css";
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import BrowsePage from "../pages/BrowsePage";
 
 function App() {
+    const [movies, setMovies] = useState([]);
+    const [tvSeries, setTVSeries] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/movies")
+            .then((res) => {
+                return res.json();
+            })
+            .then(json => {
+                setMovies(json);
+            })
+            .catch((err) => {
+                console.log(`Error: ${err}`);
+            })
+    }, [])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/TV")
+            .then((res) => {
+                return res.json();
+            })
+            .then(json => {
+                setTVSeries(json);
+            })
+            .catch((err) => {
+                console.log(`Error: ${err}`);
+            })
+    }, [])
+
     return (
         <Router>
             <Switch>
@@ -10,9 +41,9 @@ function App() {
                     <HomePage />
                 </Route>
 
-                {/* <Route path="/about">
-                    <AboutUsPage />
-                </Route> */}
+                <Route path="/browse">
+                    <BrowsePage />
+                </Route>
             </Switch>
         </Router>
     );
