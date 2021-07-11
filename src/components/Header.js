25af19380906311b7a4a3e8a6d5b7c60514e4373
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-07-11 06:00:45 -0400

    Add register modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,15 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 //import Form components
 import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 
 const Header = () => {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleLoginShow = () => setShow(true);
+    const [showSignIn, setSignInShow] = useState(false);
+    const [showRegister, setRegisterShow] = useState(false);
+    const handleSignInClose = () => setSignInShow(false);
+    const handleSignInShow = () => setSignInShow(true);
+    const handleRegisterClose = () => setRegisterShow(false);
+    const handleRegisterShow = () => setRegisterShow(true);
 
     return (
         <>
@@ -29,22 +32,22 @@ const Header = () => {
                         <Nav.Link as={Link} to="/browse">Browse</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link>Register</Nav.Link>
-                        <Nav.Link onClick={handleLoginShow}>Sign In</Nav.Link>
+                        <Nav.Link onClick={handleRegisterShow}>Register</Nav.Link>
+                        <Nav.Link onClick={handleSignInShow}>Sign In</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
 
-            {/* Login Modal */}
+            {/* Sign in Modal */}
             <Modal
-                show={show}
-                onHide={handleClose}
+                show={showSignIn}
+                onHide={handleSignInClose}
                 centered
                 dialogClassName="modal-50w">
                 <Modal.Header className="rm-bt-pd" closeButton>
                     <Modal.Title>Sign In</Modal.Title>
                 </Modal.Header>
-                <Modal.Body className="rm-tp-pdrm-bt-pd">
+                <Modal.Body>
                     <Form>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
@@ -57,13 +60,65 @@ const Header = () => {
                     </Form>
                 </Modal.Body>
                 <Modal.Footer className="rm-tp-pd">
-                    <Button className="w-100" variant="primary" onClick={handleClose}>
+                    <Button className="w-100" variant="primary" type="submit" onClick={handleSignInClose}>
                         Login
                     </Button>
                     <Form.Group className="float-left" controlId="formBasicCheckbox">
                         <Form.Check defaultChecked={true} type="checkbox" label="Remember Me" />
                     </Form.Group>
                     <p className="ml-max float-right">Forgot your password?</p>
+                </Modal.Footer>
+            </Modal>
+
+            {/* Register Modal */}
+            <Modal
+                show={showRegister}
+                onHide={handleRegisterClose}
+                centered
+                dialogClassName="modal-50w">
+                <Modal.Header className="rm-bt-pd" closeButton>
+                    <Modal.Title>Register</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridFirstName">
+                                <Form.Label>First name</Form.Label>
+                                <Form.Control placeholder="First name" />
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formGridLastName">
+                                <Form.Label>Last name</Form.Label>
+                                <Form.Control placeholder="Last name" />
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Group controlId="formGridUsername">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control placeholder="Username" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formGridPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control aria-describedby="passwordHelpBlock" placeholder="Password" />
+                            <Form.Text id="passwordHelpBlock" muted>
+                                Your password must be 8-20 characters long.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Confirm password</Form.Label>
+                            <Form.Control placeholder="Password" />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer className="rm-tp-pd">
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="I agree to the Terms and Conditions" />
+                    </Form.Group>
+
+                    <Button className="w-100" variant="primary" type="submit" onClick={handleRegisterClose}>
+                        Register
+                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
