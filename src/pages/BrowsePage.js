910254a6a0944ea56774f6d4ef89597d3c85e4ce
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-07-11 13:06:31 -0400

    Display all movies and TV shows with ObjectContainer

--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -1,11 +1,17 @@
-import React from "react";
+import { React, useContext } from "react";
+import dataContext from "../context/dataContext"
+import ObjectContainer from "../components/ObjectContainer";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const BrowsePage = () => {
+    const { movies, tvSeries } = useContext(dataContext);
     return (
         <div>
             <Header />
-            <h1>Browse</h1>
+            <ObjectContainer type={"Movies"} data={movies} />
+            <ObjectContainer type={"TV Shows"} data={tvSeries} />
+            <Footer />
         </div>
     )
 }
