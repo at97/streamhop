17b57684aa66a6deb133b61f6998afb371b4f30d
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-07-11 22:42:50 -0400

    Direct image to details page

--- src/components/ObjectContainer.js
+++ src/components/ObjectContainer.js
@@ -2,8 +2,12 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col"
+import { Link } from "react-router-dom";
 
 const ObjectContainer = (props) => {
+    // Check if container holds movies
+    const isMovie = props.type === "Movies" ? true : false;
+
     return (
         <Container fluid>
             <h2 className="ml-5 featured-text">{props.type}</h2>
@@ -11,10 +15,13 @@ const ObjectContainer = (props) => {
                 <Row xs={2} md={4} lg={6} className="item-poster-row">
                     {props.data.map((item) => (
                         <Col>
-                            <img className="item-poster"
-                                src={`/images/${item.poster_path}`}
-                                alt={`${item.title}`}
-                            />
+                            {/* Link to /movie if the container hold movies. Otherwise, link to /tv */}
+                            <Link to={(isMovie ? `/movie/${item.id}` : `/tv/${item.id}`)}>
+                                <img className="item-poster"
+                                    src={`/images/${item.poster_path}`}
+                                    alt={`${item.title}`}
+                                />
+                            </Link>
                         </Col>
                     ))}
                 </Row>
