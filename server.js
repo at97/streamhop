f870e996f817caf788623610af1995231121529b
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-07-11 23:48:43 -0400

    Update ports

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
-const middleware = jsonServer.defaults({
-    static: './build'
-});
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4000;
 
-const PORT = process.env.PORT || 5000;
-server.use(middleware);
-server.use(jsonServer.rewriter({
-    '/*': '/$1',
-}))
+server.use(middlewares);
 server.use(router);
-server.listen(PORT, () => {
-    console.log('Server is running');
-});
+
+server.listen(port);
