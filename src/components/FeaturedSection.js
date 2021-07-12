d24161758d3ee3bc134ab6ad918c28552d581867
TOPAdmin <35437308+at97@users.noreply.github.com>
2021-07-11 22:40:34 -0400

    Direct link to movie/TV show page

--- src/components/FeaturedSection.js
+++ src/components/FeaturedSection.js
@@ -2,19 +2,26 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Link } from "react-router-dom";
 
 const FeaturedSection = (props) => {
+    // Check if container holds movies
+    const isMovie = props.type === "Movies" ? true : false;
+
     return (
         <Container fluid>
             <h2 className="ml-5 featured-text">Featured {props.type}</h2>
             <Container fluid>
                 <Row xs={2} md={4} lg={6} className="item-poster-row">
                     {props.data.map((item) => (
-                        <Col>
-                            <img className="item-poster"
-                                src={`/images/${item.poster_path}`}
-                                alt={`${item.title}`}
-                            />
+                        <Col key={item.id}>
+                            {/* Link to /movie if the container hold movies. Otherwise, link to /tv */}
+                            <Link to={(isMovie ? `/movie/${item.id}` : `/tv/${item.id}`)}>
+                                <img className="item-poster"
+                                    src={`/images/${item.poster_path}`}
+                                    alt={`${item.title}`}
+                                />
+                            </Link>
                         </Col>
                     ))}
                 </Row>
